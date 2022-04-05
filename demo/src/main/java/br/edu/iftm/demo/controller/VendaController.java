e4b743e2ca9f1428559091d73e622dfaf310544d
aluno <aluno@udicentro.local>
2022-04-04 22:28:50 -0300

    Ajuste

--- demo/src/main/java/br/edu/iftm/demo/controller/VendaController.java
+++ demo/src/main/java/br/edu/iftm/demo/controller/VendaController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.iftm.demo.model.Venda;
 import br.edu.iftm.demo.service.VendaService;
 
-@RestController("venda")
+@RestController
+@RequestMapping("venda")
 public class VendaController {
     
     @Autowired
--- demo/src/main/java/br/edu/iftm/demo/service/VendaService.java
+++ demo/src/main/java/br/edu/iftm/demo/service/VendaService.java
@@ -3,10 +3,12 @@ package br.edu.iftm.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.edu.iftm.demo.model.Venda;
 import br.edu.iftm.demo.repository.VendaRepository;
 
+@Service
 public class VendaService {
 
     @Autowired
